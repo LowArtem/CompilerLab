f71c2120c5775e7b565ac6545efa295d403b12e5
Liksim <maksimzimin2020@yande.ru>
2024-01-29 22:15:25 +0300

    Исправление stmt в treePrinter

--- tree/treePrinter.cpp
+++ tree/treePrinter.cpp
@@ -350,8 +350,9 @@ void treePrinter::printStmtNode(stmtNode *node)
         }
         else if (node->type == stmtType::method_function_call_type_stmt)
         {
-            outfile << "stmt_" << node->id_node << " -> parent_" << node->id_node << ";\n";
-            outfile << "parent_" << node->id_node << " -> " << node->parent_id << ";\n";
+            outfile << "stmt_" << node->id_node << " -> expr_" << node->parent->id << ";\n";
+            printExprNode(node->parent);
+
             outfile << "stmt_" << node->id_node << " -> " << node->id << ";\n";
 
             for (auto it = node->params->begin(); it != node->params->end(); it++)
